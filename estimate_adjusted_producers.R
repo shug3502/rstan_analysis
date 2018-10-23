0f137534784caf17b121151356c4462cc53cc333
Jonathan Harrison <harrison@maths.ox.ac.uk>
2018-10-23 10:08:22 +0100

    Minor change to missing data model - unsuccessfully

--- estimate_adjusted_producers.R
+++ estimate_adjusted_producers.R
@@ -17,7 +17,7 @@ estimate_adjusted_producers <- function(nTestOE) {
   nascent_transcription = get_nascent_transcription(nTestOE,'OE')
   nt_df_oe <- data.frame(nascent_transcription) 
   colnames(nt_df_oe) = seq_len(16)
-  nt_df_oe %<>%
+  nt_df_oe <- nt_df_oe %>%
     mutate(time = times$ts4) %>%
     gather('cellID','transcription',-time) %>%
     mutate(cellID=as.integer(cellID))
@@ -30,7 +30,7 @@ estimate_adjusted_producers <- function(nTestOE) {
     as.matrix()
   stan_file <- "missing_data_poisson_transcription.stan"
   stan_list <- list(T=nTestOE,
-                    transcription_obs=obs_transcription
+                    transcription_obs=obs_transcription[,2:16]
   )
   estimates <- stan(file = stan_file,
                     data = stan_list,
@@ -41,7 +41,6 @@ estimate_adjusted_producers <- function(nTestOE) {
   )
   estimated_producers = rstan::extract(estimates,pars='a',permuted=TRUE)[[1]] %>% apply(.,2,median) 
   estimated_producers = 2*15*estimated_producers/sum(estimated_producers)
-  producers = matrix(rep(estimated_producers,nTestOE),ncol=16)
-  producers[,1]=0
+  producers = cbind(rep(0,nTestOE),matrix(rep(estimated_producers,nTestOE),ncol=16))
   return(producers)
 }
